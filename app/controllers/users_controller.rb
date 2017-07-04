d454ef8b046e3de1534f86522d48c24ebf3900ce
Yuta Gunji <yuta.g05@gmail.com>
2017-07-04 20:12:50 +0900

    Modified redirect_ to path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
-      redirect_to controller: :messages, action: :index
+      redirect_to root_path
     else
       render :edit
     end
