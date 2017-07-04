5d25d33a17ecb531ea6a07572d8d2154f65cf264
Yuta Gunji <yuta.g05@gmail.com>
2017-07-04 15:52:56 +0900

    Define update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,11 @@ class UsersController < ApplicationController
 
   def edit
   end
-  
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params) if user.id == current_user.id
+    redirect_to root_path
+  end
+
 end
