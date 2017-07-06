1f48b4ee24466690b6429ce4e0b8b906dfdb9b9a
Yuta Gunji <yuta.g05@gmail.com>
2017-07-06 20:58:52 +0900

    Add uniqueness validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   has_many :members
   has_many :users, through: :members
 end
