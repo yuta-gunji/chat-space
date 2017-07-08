8a458e9222e3b0279f21a9d9a2237bb2f2dda8eb
Yuta Gunji <yuta.g05@gmail.com>
2017-07-08 17:46:27 +0900

    Add flash message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_group
+  before_action :set_group_set_messages
 
   def index
     @message = Message.new
@@ -10,6 +10,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to group_messages_path
     else
+      flash.now[:alert] = "メッセージを入力してください"
       render :index
     end
   end
@@ -20,7 +21,8 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 
-  def set_group
+  def set_group_set_messages
     @group = Group.find(params[:group_id])
+    @messages = @group.messages.includes(:user)
   end
 end
