b829eb00170e2e373888b13e235339e3a1f8de1c
Yuta Gunji <yuta.g05@gmail.com>
2017-07-13 13:57:02 +0900

    Modified test code to bundle process

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -2,17 +2,15 @@ require 'rails_helper'
 
 describe MessagesController, type: :controller do
   let(:user) { create(:user, :for_controller) }
-
-  context "when user log-in" do
-    login_user
-    describe 'GET #index' do
+  let(:group) { build(:group, id: 1) }
+  describe 'GET #index' do
+    context "when user log-in" do
+      login_user
       it "assigns the requested message to @message" do
-        group = build(:group, id: 1)
         get :index, params: { group_id: group.id }
         expect(assigns(:message)).to be_a_new(Message)
       end
       it "assigns the requested group to @group" do
-        group = build(:group, id: 1)
         get :index, params: { group_id: group.id }
         expect(assigns(:group)).to eq group
       end
@@ -27,45 +25,42 @@ describe MessagesController, type: :controller do
         expect(assigns(:messages)).to eq messages
       end
       it "renders the index tempalte" do
-        group = build(:group, id: 1)
         get :index, params: { group_id: group.id }
         expect(response).to render_template :index
       end
     end
-    describe 'POST #create' do
-      let(:group) { user.groups.first }
-      it "saves the new message in the database" do
-        expect{
-          post :create, params: { group_id: group.id, message: attributes_for(:message) }
-        }.to change(Message, :count).by(1)
-      end
-      it "redirects to the messages#index" do
-        post :create, params: { group_id: group.id, message: attributes_for(:message) }
-          expect(response).to redirect_to group_messages_path
-      end
-      it "doesn't save the new message in the database" do
-        expect{
-          post :create, params: { group_id: group.id, message: attributes_for(:message, body: nil, image: nil) }
-        }.not_to change(Message, :count)
-      end
-      it "renders the index tempalte when can't save the new message" do
-        post :create, params: { group_id: group.id, message: attributes_for(:message, body: nil, image: nil) }
-        expect(response).to render_template :index
-      end
-    end
-  end
-
-  context "when user not log-in" do
-    describe 'GET #index' do
+    context "when user not log-in" do
       it "redirect to sign-up view" do
-        group = build(:group, id: 1)
         get :index, params: {group_id: group.id}
         expect(response).to redirect_to new_user_session_path
       end
     end
-    describe 'POST #create' do
+  end
+  describe 'POST #create' do
+    context "when user log-in" do
+      let(:firts_group) { user.groups.first }
+      login_user
+      it "saves the new message in the database" do
+        expect{
+          post :create, params: { group_id: firts_group.id, message: attributes_for(:message) }
+        }.to change(Message, :count).by(1)
+      end
+      it "redirects to the messages#index" do
+        post :create, params: { group_id: firts_group.id, message: attributes_for(:message) }
+          expect(response).to redirect_to group_messages_path
+      end
+      it "doesn't save the new message in the database" do
+        expect{
+          post :create, params: { group_id: firts_group.id, message: attributes_for(:message, body: nil, image: nil) }
+        }.not_to change(Message, :count)
+      end
+      it "renders the index tempalte when can't save the new message" do
+        post :create, params: { group_id: firts_group.id, message: attributes_for(:message, body: nil, image: nil) }
+        expect(response).to render_template :index
+      end
+    end
+    context "when user not log-in" do
       it "redirect to sign-up view" do
-        group = build(:group, id: 1)
         post :create, params: {group_id: group.id}
         expect(response).to redirect_to new_user_session_path
       end
