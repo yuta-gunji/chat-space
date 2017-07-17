c0e3a3f497e6a92e1602b3c5f4d48c1f588affc5
Yuta Gunji <yuta.g05@gmail.com>
2017-07-17 19:23:00 +0900

    Define index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   private
 
   def user_params
